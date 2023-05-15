cd4f6578c316a05c4232793cc6418d06eceff6d9
ChrizzenZander <christofferssander@gmail.com>
2023-05-15 12:07:44 +0200

    SceneManagement works now

--- Unity Project/ジャミフ/Assets/Scripts/MainMenu.cs
+++ Unity Project/ジャミフ/Assets/Scripts/MainMenu.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class QuitApplication : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Final Game");
+    }
     public void QuitGame()
     {
         Application.Quit();
